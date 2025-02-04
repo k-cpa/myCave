19af1de8c2c488397cec966d8d6c6759c3a02b5c
Kev <kevin@MacBook-Pro-de-Kev.local>
2025-02-04 17:15:56 +0100

    forget to change the userController name

--- src/Controller/UserSectionController.php
+++ src/Controller/UserSectionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class UserController extends AbstractController{
+final class UserSectionController extends AbstractController{
 
     // TEMPLATE CAVE PERSONNELLE DE L'UTILISATEUR 
 
